3c154a0ff19b394baedd2c94ba3399435a7bf3d7
jmarmol1013 <jmarmol1@my.centennialcollege.com>
2022-10-12 14:31:09 -0400

    Internal Documentation

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,4 @@
+/* Portfolio Juan Camilo Marmolejo 301223791 01/10/2022 */
 window.addEventListener("scroll",()=>{
     let nav = document.querySelector("nav");
     nav.classList.toggle("fixed",window.scrollY > 1000);
